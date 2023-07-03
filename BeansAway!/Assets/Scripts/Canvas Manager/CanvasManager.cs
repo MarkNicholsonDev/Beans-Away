8440906baea7e1872749aeb541af9be0649af944
Mark Nicholson <123469179+MarkNicholsonDev@users.noreply.github.com>
2023-07-03 14:57:06 +0100

    Set up the Canvas manager/Menu

---
+++ BeansAway!/Assets/Scripts/Canvas Manager/CanvasManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum CanvasType { 
+    MainMenu,
+    GameUI,
+    EndScreen
+}
+public class CanvasManager : Singleton<CanvasManager>
+{
+    private List<CanvasController> canvasControllerList;
+    private CanvasController lastActiveCanvas;
+    protected override void Awake()
+    {
+        canvasControllerList = GetComponentsInChildren<CanvasController>().ToList();
+
+        canvasControllerList.ForEach(x => x.gameObject.SetActive(false));
+        SwitchCanvas(CanvasType.MainMenu);
+    }
+
+    public void SwitchCanvas(CanvasType newCanvas)
+    {
+        if (lastActiveCanvas != null)
+        {
+            lastActiveCanvas.gameObject.SetActive(false);
+        }
+        else { Debug.LogWarning("The last active canvas wasn't found!"); }
+
+        CanvasController desiredCanvas = canvasControllerList.Find(x => x.canvasType == newCanvas);
+        if (desiredCanvas)
+        {
+            desiredCanvas.gameObject.SetActive(true);
+            lastActiveCanvas = desiredCanvas;
+        }
+        else { Debug.LogWarning("The desired canvas wasn't found!"); }
+    }
+}
---
+++ BeansAway!/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    // Start is called before the first frame update
+    CanvasManager canvasManager;
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
